da647d6df1bb34c60946059d422fad5ac9288837
mauboro <trying.again999@outlook.com>
2023-04-17 14:45:43 -0300

    adding the completed version of the linked list exercise

--- eloquent_javascript/data_structures_objects_and_arrays/exercises/a_list/main.js
+++ eloquent_javascript/data_structures_objects_and_arrays/exercises/a_list/main.js
@@ -8,17 +8,37 @@ function arrayToList(array){
 		list = {value: array[i], rest: list};
 		}
 	}
-	console.log(list);
+	return list;
 }
 
 function listToArray(list){
+	let res = [];
 	for (let node = list; node; node = node.rest){
-		console.log(node);
+		res.push(node.value);
+	}
+	return res;
+}
+
+function prepend(value, rest){
+	return {value, rest};
+}
+
+function nth(linkedlist, value){
+	let count = 0;
+	for (let node = linkedlist; node; node = node.rest){
+		if (node.value === value){
+			return count;
+		}else{
+			count += 1;
+		}
 	}
 }
 
-
-arrayToList([1, 2, 3, 4, 5]);
-let list = arrayToList([1, 2, 3])
-listToArray(list);
-
+// arrayToList([1, 2, 3, 4, 5]);
+// let list = arrayToList([1, 2, 3])
+// console.log(list);
+// let prepended = prepend(0, list);
+// console.log(prepended);
+// let array = listToArray(list);
+// console.log(array);
+// console.log(nth(list, 3))
