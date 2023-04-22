199a236fa612c1491ae2e3e83d41afe2efbc3ced
mauboro <trying.again999@outlook.com>
2023-04-22 19:16:53 -0300

    adding the exercise made today

--- eloquent_javascript/data_structures_objects_and_arrays/exercises/deep_comparison/main.js
+++ eloquent_javascript/data_structures_objects_and_arrays/exercises/deep_comparison/main.js
@@ -1,10 +1,16 @@
+// it worked gracefully but I am almost sure that I've cheated the exercise
+
 function deepEqual(obj1, obj2){
-	console.log(Object.keys(obj1));
-	console.log(Object.keys(obj2));
 	for (let o of Object.keys(obj1)){
-		console.log(Object.keys(o));
+		if (typeof obj1[o] != typeof obj2[o]){
+			return false;
+		}
 	}
+	return true;
 }
 
 let obj = {here: {is: "an"}, object: 2}
-deepEqual(obj, obj);
+
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
